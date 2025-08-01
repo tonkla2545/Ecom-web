277f846c55d12859535c251fa7c3f7c6830cc014
tonkla2545 <109588556+tonkla2545@users.noreply.github.com>
2025-08-01 11:55:15 +0700

    helmet

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes'
 import { ToastContainer, toast } from 'react-toastify';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   // JavaScript
   return (
     <>
+      <Helmet>
+        <title>E-commerce</title>
+      </Helmet>
       <ToastContainer />
       <AppRoutes />
     </>
